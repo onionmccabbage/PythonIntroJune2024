cd2ce5becd1bb9505359ef1e5db872a577fd848b
onionmccabbage <onionmccabbage@live.com>
2024-06-17 16:15:18 +0100

    accessing JSON from an API

---
+++ using_sys.py
@@ -0,0 +1,6 @@
+# sys refers the current operating system
+import sys
+
+# we can access a list of every location that Python will look for imports
+sys.path.append('d:/utilitiesGoHere') # we can add locations where Python should look for imports
+print(sys.path)
---
+++ usng_requests.py
@@ -0,0 +1,15 @@
+# if we managed to pip install the requests library, we can...
+import requests
+
+# the requests library lets us access external APIs via http
+def getOnePhoto():
+    '''make a requetss to an API to retrieve data for one photo'''
+    apiUrl = 'https://jsonplaceholder.typicode.com/photos/3'
+    response = requests.get(apiUrl) # make a request
+    # The requests library understands JSON, XML, text etc.
+    data = response.json() # we now have the data
+    return data
+
+if __name__ == '__main__':
+    result = getOnePhoto()
+    print(result)
