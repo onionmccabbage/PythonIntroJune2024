52ef0df9d14752a18431cbc97a00ea73226d9d03
onionmccabbage <onionmccabbage@live.com>
2024-06-17 10:46:01 +0100

    using list and tuple with slicing

--- basics.py
+++ basics.py
@@ -23,3 +23,28 @@ for i in s: # the members are ordinal
     print(i) # by default the print statement always adds a new line character
 
 # when the code is no longer indented, that is the end of the code block
+
+# other collections: tuple list
+# a tuple is an immutable ordinal collection of any type
+t = (4, 6, 'hello', False, None, 'another string')
+print(type(t), t, t[3:5])
+# we can iterate
+for _ in t: # we often use _ as the iterable in Python (just a convention)
+    print(_)
+# NB tuples are immutable (we cannot change them once created)
+
+# a list is a mutable ordinal collection of any type
+l = [6, 4, 'coffee', True, t, a, b, s]
+# we may mutate a list as follows:
+l.append('this is appended to the list')
+l.insert(4, s)
+l.pop() # the last member of the list is removed
+l.remove('coffee') # remove the first instance of this item
+
+print(l[0:4])
+for _ in l:
+    print(_, end=', ') # we may choose to override the default new-line
+
+# -1 is often a valid number in slicing
+print(s[-2::-1]) # step backwards
+print(s[-3]) #changed - access the third-from-last element
