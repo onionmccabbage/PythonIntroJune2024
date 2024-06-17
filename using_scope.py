7e7da37c08b5e9af2b44591c004a395fe52480e3
onionmccabbage <onionmccabbage@live.com>
2024-06-17 16:30:56 +0100

    end of day 1

--- using_scope.py
+++ using_scope.py
@@ -1,6 +1,8 @@
 # everything that is not indented is considered to be the global scope
 g = 'this is in the global scope'
 
+# NB in Python scope and namespace can mean the same thing
+
 # anything within a code block has its own scope
 def fnA():
     global g # we now have access to the value in the global scope
