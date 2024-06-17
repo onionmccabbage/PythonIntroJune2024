7c79e2f63fef389498bbb004acaeaa2f2e607d3e
onionmccabbage <onionmccabbage@live.com>
2024-06-17 11:24:38 +0100

    dict and set

--- using_structures.py
+++ using_structures.py
@@ -8,4 +8,15 @@ d['break'] = drinks_t[0] # we have no control where in the dict this will be
 print(d, type(d), d['age']) # we cannot access the list 'l' it is not in this module
 # we can iterate over a dictionary
 for (k,v) in d.items():
-    print(k, v)
+    # print formatting
+    # print(k, v)
+    # f'   {}   ' lets us inject any value or statement into a print string
+    print( f'{k} is {d[k]} ' ) # f means format the print
+
+# a set is a collection of un-indexed unique values of any type
+my_set = {4, 7, 2, 9, True, 'anything', 4, 2, True} # curly brackets mean both a dict and a set. Context determins which
+my_set.add(False)
+my_set.add(4) # the unique member exists once
+my_set.add(None)
+my_set.add(5.431)
+print(my_set, type(my_set))
