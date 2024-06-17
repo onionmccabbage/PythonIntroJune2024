202b32d70f66793e8382cbed88ae99c1883348b5
onionmccabbage <onionmccabbage@live.com>
2024-06-17 11:39:07 +0100

    using several except blocks

--- using_input.py
+++ using_input.py
@@ -6,6 +6,10 @@ try:
     # we may cast to a different type
     n_f = float(n)
     print(n_f, type(n_f))
+# we typically handle specific exceptions first
+except ValueError as ve:
+    print(f'That cannot be cast as a float {ve}')
+# ... then generic exceptions later
 except Exception as err:
     print(f'Problem: {err}')
 finally: # this always runs
