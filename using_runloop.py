026b9691428d3f7721ddc42b9b765c67fda3f7de
onionmccabbage <onionmccabbage@live.com>
2024-06-17 12:12:03 +0100

    using random and while

---
+++ using_runloop.py
@@ -0,0 +1,31 @@
+import random # this is part of the Python standard library
+
+# it is good practice to docunment your code
+def getRandInt():
+    '''return a random integer between 1 and 10
+    this function takes no arguments'''
+    return random.randint(1,10)
+
+# we may choose to pass arguments into our function
+def guessNumber(r):
+    '''iterate until we hit the random number'''
+    for _ in range(1,11): # a range returns each value from (start, stop-before)
+        if _ == r:
+            return f'The number is {_}'
+
+def userGuess():
+    r = getRandInt() # we have another random number
+    # we could use a runloop
+    while True: # this loop will carry on endlessly
+        ask = input('guess:')
+        ask_n = float(ask) # remember - input is ALWAYS a string
+        if ask_n == r:
+            print(f'You guessed {r}')
+            break #  this stops the loop
+
+r = getRandInt() # we now have a random integer
+result = guessNumber(r) # pass in r as an argument
+
+print( result )
+
+
