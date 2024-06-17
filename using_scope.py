c25b490462bf4603fa28f74353aab5f20d717a32
onionmccabbage <onionmccabbage@live.com>
2024-06-17 16:28:54 +0100

    using global scope

--- using_scope.py
+++ using_scope.py
@@ -4,7 +4,7 @@ g = 'this is in the global scope'
 # anything within a code block has its own scope
 def fnA():
     global g # we now have access to the value in the global scope
-    g = 'this is in a local scope'
+    g = 'which scope is this?' # depends if we say 'global'
     return g
 
 if __name__ == '__main__':
