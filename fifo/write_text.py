08af9188fb84ecddcea2380843581d8b9b8fd5de
onionmccabbage <onionmccabbage@live.com>
2024-06-18 10:18:35 +0100

    write to file

--- fifo/write_text.py
+++ fifo/write_text.py
@@ -7,17 +7,24 @@
 def printTofile(s):
     '''send printed output to a text file'''
     # we need a file access object
-    fout = open('my_file.txt', 'at') # 'at' will append text
-    # by default print sends its output to the terminal console
-    print(s, file=fout) # redirect the print output to our file access object
+    try: # whenever we deal with files, there might be a problem
+        # 'at' will append text, 'xt' will exclusively write text. 'wt' will (over)write
+        fout = open('my_file.txt', 'wt') # xt fails if file exists
+        # by default print sends its output to the terminal console
+        print(s, file=fout) # redirect the print output to our file access object
+        fout.close()
+    except Exception as err:
+        print(f'There was a problem: {err}')
 
 def writeToFile(s):
     '''write text to a file'''
     fout = open('my_file.txt', 'at')
-    fout.write(s) # send our stringo f text to the file access object 'write' stream
+    fout.write(s) # send our string of text to the file access object 'write' stream
+    # fout.write(f'{s}\n') # we MAY choose to add a new line
+    fout.close() # always a good idea to tidy up
 
 if __name__ == '__main__':
     # here we exercise the local code
     words = 'Here is some text we need to persist in a file'
-    printTofile(words)
-    writeToFile(words)
+    printTofile(words) # uses print (ends with a new line )
+    writeToFile(words) # does not use print ( no default new line at the end)
