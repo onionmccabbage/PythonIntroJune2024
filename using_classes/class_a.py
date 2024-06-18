1fb2a82d805ef2f26ce5485112ae5bc28eed0c3c
onionmccabbage <onionmccabbage@live.com>
2024-06-18 16:28:26 +0100

    simple class definition

---
+++ using_classes/class_a.py
@@ -0,0 +1,16 @@
+# the most simple class in Python
+
+# in Python everything is an object
+# so every class, every list, tuple, dict, modules, packages set are all objects
+class Woobly:
+    pass
+
+if __name__ == '__main__':
+    # we may choose to create instances of any class
+    w1 = Woobly()
+    w2 = Woobly()
+    w3 = Woobly()
+    l1 = list() # we now have an instance of the list class
+    d1 = dict()
+    s1 = set()
+    a1 = str()
