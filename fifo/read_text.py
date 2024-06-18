7d9e3480a36d06679be63e600c408545d3255de1
onionmccabbage <onionmccabbage@live.com>
2024-06-18 10:41:23 +0100

    read, readline and readlines

--- fifo/read_text.py
+++ fifo/read_text.py
@@ -4,10 +4,17 @@ def readFromTextFile():
     # 'rt' will read text
     fin = open('my_file.txt', 'rt')
     # t = fin.read() # read back the entire file
-    t = fin.readline(10) # read back the line, or n characters of the first line
+    # t = fin.readline(10) # read back the line, or n characters of the first line
+    t = fin.readlines() # retrieve a list with each line as a member
+    # can we access specific content...
+    # fin.seek(8, 1) # whence may be 0 (start of file) 1 (end of file) or 2
+    # t = fin.readline() # ....
     fin.close()
     return t
 
 if __name__ == '__main__':
     txt = readFromTextFile()
     print(txt) # see what we have
+    # print(type(txt))
+    # for _ in txt:
+    #     print(_)
